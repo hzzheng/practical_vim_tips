e410c7fce1cbc3962194a23e3c65b649f0d5c7b3
zhengchao <zhchaozju@gmail.com>
2022-07-24 12:06:37 +0800

    feat: commit first tip

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,23 @@
+export default {
+  title: 'Practical Vim Tips',
+  themeConfig: {
+    nav: [
+      {
+        text: 'The Book',
+        link: 'https://book.douban.com/subject/25869486/',
+      },
+    ],
+    sidebar: [
+      {
+        text: 'Chapter 1',
+        collapsible: true,
+        items: [
+          {
+            text: 'tip 001',
+            link: '/tip_001',
+          },
+        ],
+      },
+    ],
+  },
+}
--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,7 @@
-# Hello VitePress
+# Tips & Snippts From Practical Vim
+
+---
+
+Tips & snippts extracted from the book `Practical Vim` for personal daily practice.
+
+You can visit this [reference](https://book.douban.com/subject/25869486/) to view the book.
