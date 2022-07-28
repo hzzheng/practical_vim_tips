c980552174b784d4b2c1aa33356f2c369ab29f6d
zhengchao <zhchaozju@gmail.com>
2022-07-28 13:49:58 +0800

    chore: update

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,3 +1,59 @@
+const chapter_01 = {
+  text: 'Chapter 1',
+  collapsible: true,
+  items: [
+    {
+      text: 'tip 001',
+      link: '/tip_001',
+    },
+    {
+      text: 'tip 002',
+      link: '/tip_002',
+    },
+    {
+      text: 'tip 003',
+      link: '/tip_003',
+    },
+    {
+      text: 'tip 004',
+      link: '/tip_004',
+    },
+    {
+      text: 'tip 005',
+      link: '/tip_005',
+    },
+    {
+      text: 'tip 006',
+      link: '/tip_006',
+    },
+  ],
+}
+const chapter_02 = {
+  text: 'Chapter 2',
+  collapsible: true,
+  items: [
+    {
+      text: 'tip 007',
+      link: '/tip_007',
+    },
+    {
+      text: 'tip 008',
+      link: '/tip_008',
+    },
+    {
+      text: 'tip 009',
+      link: '/tip_009',
+    },
+    {
+      text: 'tip 0010',
+      link: '/tip_010',
+    },
+    {
+      text: 'tip 0011',
+      link: '/tip_011',
+    },
+  ],
+}
 export default {
   title: 'Practical Vim Tips',
   themeConfig: {
@@ -7,63 +63,6 @@ export default {
         link: 'https://book.douban.com/subject/25869486/',
       },
     ],
-    sidebar: [
-      {
-        text: 'Chapter 1',
-        collapsible: true,
-        items: [
-          {
-            text: 'tip 001',
-            link: '/tip_001',
-          },
-          {
-            text: 'tip 002',
-            link: '/tip_002',
-          },
-          {
-            text: 'tip 003',
-            link: '/tip_003',
-          },
-          {
-            text: 'tip 004',
-            link: '/tip_004',
-          },
-          {
-            text: 'tip 005',
-            link: '/tip_005',
-          },
-          {
-            text: 'tip 006',
-            link: '/tip_006',
-          },
-        ],
-      },
-      {
-        text: 'Chapter 2',
-        collapsible: true,
-        items: [
-          {
-            text: 'tip 007',
-            link: '/tip_007',
-          },
-          {
-            text: 'tip 008',
-            link: '/tip_008',
-          },
-          {
-            text: 'tip 009',
-            link: '/tip_009',
-          },
-          {
-            text: 'tip 0010',
-            link: '/tip_010',
-          },
-          {
-            text: 'tip 0011',
-            link: '/tip_011',
-          },
-        ],
-      },
-    ],
+    sidebar: [chapter_01, chapter_02],
   },
 }
